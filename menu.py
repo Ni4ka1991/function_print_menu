0b5a52212c53a686cad88a5c780ee19246a11dae
Ni4ka1991 <astahova_nica@mail.ru>
2021-03-08 17:51:09 +0200

    Add file menu.py

---
+++ menu.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+price_pizza_mdl = 75
+price_latte_mdl = 25
+price_salmon_steak_1_portion_mdl = 125
+
+name_pizza = "Pizza Diverso"
+name_drink = "Latte"
+name_eat = "Grilled Salmon"
+
+print("\u2065"*28)
+
+print("1. ", name_pizza, " : ", price_pizza_mdl, " MDL")
+print("2. ", name_eat ,  ": ", price_salmon_steak_1_portion_mdl, "MDL")
+print("3. ", name_drink, "         : ", price_latte_mdl, " MDL")
+
+print("\u2065"*28)
